800ea1361ab4c717a28d3a58d7565a19e4393c54
deblaseroman <79052824+deblaseroman@users.noreply.github.com>
2024-09-15 23:32:25 -0400

    create updatedStock function

--- inventory.js
+++ inventory.js
@@ -24,11 +24,29 @@ const inventory =
 }
 }
 displayProuctDetails(inventory[0]);
-
+// Create displayProductDetails function
 var inventory = [
 { name: "laptop", price: 500, quantity: 10, lowStockLevel: 3},
 { name: "mouse", price: 70, quantity: 30, lowStockLevel: 5},
 { name: "keyboard", price: 100, quantity: 25, lowStockLevel: 5,},
 { name: "monitor", price: 250, quantity: 20, lowStockLevel: 5},
 { name: "speaker", price: 150, quantity: 20, lowStockLevel: 5}
-]
+];
+function updateStock(product, unitsSold){
+    product.quantity = product.quantity - unitsSold;
+    var stockStatus;
+    if (product.quantity <= 0);{
+        stockStatus = "out of stock";
+    }
+     if(product.quantity <= product.lowStockLevel){
+        stockStatus = "low stock";
+    } else {
+        stockStatus = "in stock";
+    }
+    console.log('Product:' + product.name);
+    console.log('quantity left:' + product.quantity);
+    console.log('stock status:' + stockStatus);
+
+// create updatedStock function
+
+}
