ccde6cda717038bde53f38a22c5961ee41279df8
deblaseroman <79052824+deblaseroman@users.noreply.github.com>
2024-09-15 22:24:11 -0400

    Create an Inventory Array of Product Objects

---
+++ inventory.js
@@ -0,0 +1,12 @@
+{
+
+const inventory = 
+[
+{ name: "laptop", price: 500, quantity: 10, lowStockLevel: 3},
+{ name: "mouse", price: 70, quantity: 30, lowStockLevel: 5},
+{ name: "keyboard", price: 100, quantity: 25, lowStockLevel: 5,},
+{ name: "monitor", price: 250, quantity: 20, lowStockLevel: 5},
+{ name: "speaker", price: 150, quantity: 20, lowStockLevel: 5}
+]
+// initalize inventory with product objects
+}
